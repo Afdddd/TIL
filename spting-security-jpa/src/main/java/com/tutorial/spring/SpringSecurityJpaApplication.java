e5586b636a4a93bd6803f0c9653c9c10a54d94ff
Afdddd <kimdlsduq36@gmail.com>
2025-01-18 17:49:10 +0900

    feat : project init

---
+++ spting-security-jpa/src/main/java/com/tutorial/spring/SpringSecurityJpaApplication.java
@@ -0,0 +1,12 @@
+package com.tutorial.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJpaApplication.class, args);
+    }
+}
