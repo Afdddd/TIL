1a12e2dd36bb33704e833e0bface0ec54ab2e10c
Afdddd <kimdlsduq36@gmail.com>
2025-01-18 17:51:34 +0900

    chore : security 임시 설정
    csrf 비활성
    모든요청 허용
    기본 login 비활성

---
+++ spting-security-jpa/src/main/java/com/tutorial/spring/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.tutorial.spring.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
